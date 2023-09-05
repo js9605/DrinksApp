01739288617460787caa6f32def84182b7b968b5
Jacek <jacekszwarc1@gmail.com>
2023-09-05 19:12:25 +0200

    Add CRUD
    Configure POST and GET requests

--- drinks/views.py
+++ drinks/views.py
@@ -1,13 +1,43 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
+@api_view(['GET', 'POST'])
 def drink_list(request):
-    drinks = Drink.objects.all()    
-    serializer = DrinkSerializer(drinks, many=True)
-    return JsonResponse({"drinks" : serializer.data}, safe=False)
+
+    if request.method == 'GET':
+        #get all the drinks
+        #serialize them
+        #get json
+        
+        drinks = Drink.objects.all()    
+        serializer = DrinkSerializer(drinks, many=True)
+        return JsonResponse({"drinks" : serializer.data})
     
-    #get all the drinks
-    #serialize them
-    #get json
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, id):
+
+    try: 
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        pass
+    elif request.method == 'DELETE':
+        pass
